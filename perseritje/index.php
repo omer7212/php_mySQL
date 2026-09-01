e3ff892c8d0eab5249f8b15516ddc615878bf86e
omer7212 <omershala77@gmail.com>
2026-09-01 17:46:13 +0200

    perseritje

---
+++ perseritje/index.php
@@ -0,0 +1,56 @@
+  <?php
+
+$num =30;
+
+function bmi($m,$h){
+
+    $bmi = $m/($h*$h);
+    return $bmi;
+
+
+}
+
+
+echo bmi(70,1.8);
+$result = bmi(70,1.8);
+echo $result;
+
+
+
+?>
+
+<?php
+$age=15;
+  
+if($age<18){
+    echo 'Minor';
+}else if ($age==18){
+    echo 'You are 18';
+}else{
+    echo 'You are an adult';
+}
+
+
+?> 
+ 
+
+
+<?php 
+
+$multi_arr=[["Basketball", "Football",5,"Esports"],[54,657,54,12]];
+echo count ($multi_arr[1])."<hr>";
+
+for($i=0;$i<count($multi_arr);$i++){
+    for($j=0;$j<count($multi_arr[$i]);$j++){
+        echo $multi_arr[$i][$j]."</br?";
+    }
+}
+
+
+foreach($multi_arr[0] as $sport){
+    echo "</br>" .$sport;
+}
+
+?>
+
+
