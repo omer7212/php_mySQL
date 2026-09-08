5c82a5e07ab428a63228322a9ff86fb51516078d
omer7212 <omershala77@gmail.com>
2026-09-08 17:47:40 +0200

    Module9

---
+++ Module9/add.php
@@ -0,0 +1,24 @@
+<?php
+    //With this file we include the database connection
+    include_once("index.php");
+
+    if(isset($_POST['submit'])){
+        $username=$_POST['uname'];
+        $password=$_POST['passwordi'];
+        $age=$_POST['age'];
+
+        //We encrypt the password to hide details of password
+
+        $hashed_password=password_hash($password,PASSWORD_BCRYPT);
+
+        $sql="INSERT INTO users(username,password,age) VALUES ('$username','$hashed_password',$age)";
+
+
+        $conn->exec($sql);
+        echo "New record created successfully!";
+    }
+
+
+
+
+?>
---
+++ Module9/index.php
@@ -0,0 +1,20 @@
+<?php 
+$host='localhost';
+$db_name='testdb';
+$user='root';
+$password='';
+
+
+try{
+    $conn=new PDO("mysql:host=$host;dbname=$db_name",'root',"");
+    echo "Connected succesfully!";
+
+
+}catch(Exception $error){
+    echo $error;
+}
+
+
+
+
+?>
